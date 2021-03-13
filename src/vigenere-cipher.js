f65c2ba64f071e189d3a1b36ab5383c6dd86703b
Yelizaveta Smaliakova <smolyakova580@gmail.com>
2021-03-13 17:32:14 +0300

    All tests passed

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,48 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }
+    constructor(type) {
+        if (type === false) this.type = false;
+        else this.type = true;
+    }
+
+    encrypt(message, key) {
+        if (message === "" || key === "") throw "Error";
+
+        let encrypt_message = "";
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        let j = 0;
+        for (let i = 0; i < message.length; i++) {
+            if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+                let newletter = (message.charCodeAt(i) + key.charCodeAt(j % key.length) - 65 * 2) % 26 + 65;
+                encrypt_message += String.fromCharCode(newletter);
+                j++;
+            }
+            else encrypt_message += message[i];
+        }
+        if (this.type) return encrypt_message;
+        else return encrypt_message.split('').reverse().join('');
+    }
+
+    decrypt(message, key) {
+        if (message === "" || key === "") throw "Error";
+
+        let decrypt_message = "";
+        message = message.toUpperCase();
+        key = key.toUpperCase();
+        let j = 0;
+        for (let i = 0; i < message.length; i++) {
+            if (message.charCodeAt(i) >= 65 && message.charCodeAt(i) <= 90) {
+                let newletter = (message.charCodeAt(i) + 26 - key.charCodeAt(j % key.length)) % 26 + 65;
+                decrypt_message += String.fromCharCode(newletter);
+                j++;
+            }
+            else decrypt_message += message[i];
+        }
+        if (this.type) return decrypt_message;
+        else return decrypt_message.split('').reverse().join('');
+    }
 }
 
 module.exports = VigenereCipheringMachine;
